fcd5398d3a9cfe18e7543a3eb354e2de551f9231
Andrey Michaluik <andrey.michaluik@sigmaukraine.com>
2013-10-09 18:00:29 +0300

    update libs

--- NLog.MongoDB/Properties/AssemblyInfo.cs
+++ NLog.MongoDB/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Logrythmik")]
 [assembly: AssemblyProduct("NLog.MongoDB")]
-[assembly: AssemblyCopyright("Copyright © Logrythmik 2012")]
+[assembly: AssemblyCopyright("Copyright © Logrythmik 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyVersion("0.8.0")]
+[assembly: AssemblyFileVersion("0.8.0")]
